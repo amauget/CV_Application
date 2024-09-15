2621b2b15cd6f3b630b6b6f660ca3f63bbd1c3e3
AaronMauget <amauget@hotmail.com>
2024-09-15 15:25:52 -0700

    Complete the logic for editing the header. First attempt is localized to header. Now, branching out to use methods in Form function Form function to be used by all page functions, including header.

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -10,50 +10,100 @@ export default function Heading(){
   const [edit, toggleEdit] = useState(false)
 
   const [editData, updateData] = useState({
-    name: {item: name, update: updateName }, 
-    source: { item: source, update: updateSource}, 
-    alt: {item: alt, update: updateAlt}, 
-    phoneNumber: {item: phoneNumber, update: updatePhoneNumber}, 
-    email: {item: email, update: updateEmail}, 
-    edit: {item: edit, update: toggleEdit}
+    //item is value, key is for identifying during loop iteration, update is state function
+    name: {item: name, key:'name', update: updateName }, 
+    source: { item: source, key: 'source', update: updateSource}, 
+    alt: {item: alt, key:'alt', update: updateAlt}, 
+    phoneNumber: {item: phoneNumber, key:'phoneNumber', update: updatePhoneNumber}, 
+    email: {item: email, key:'email', update: updateEmail}, 
   }
   )
+  const [formData, setFormData] = useState({ //temporary state to hold form data before submit
+    name: name,
+    source: source,
+    alt: alt,
+    phoneNumber: phoneNumber,
+    email:email
+
+  })
   
   if(edit === true){
+  
+
+    const handleInputChange = (e) =>{
+      const {name, value} = e.target
+
+      setFormData({
+        ...formData,
+        [name]: value
+      })
+    }
+    const handleSubmit = (e) =>{
+      e.preventDefault()
+
+      for(let i in editData){
+        let key = editData[i].key 
+        //iterates through list to define each key
+        
+        editData[key].update(formData[key])
+        //targets the state function within list and updates.
+
+      }
+      toggleEdit(false)
+    }
+    const handleCancel = (e) =>{
+      e.preventDefault()
+      
+      toggleEdit(false)
+    }
     return(
-      <Form
-       editData={editData}
-       />
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" name='name' value={formData.name} onChange={handleInputChange}/>
+      
+        <label htmlFor="source">Image URL:</label>
+        <input type="text" id="source" name='source' value={formData.source} onChange={handleInputChange}/>
+
+        <label htmlFor="alt">Image Alt Text:</label>
+        <input type="text" id="alt" name='alt' value={formData.alt} onChange={handleInputChange}/>
+
+        <label htmlFor="phoneNumber">Phone Number:</label>
+        <input type="text" id="phoneNumber" name='phoneNumber' value={formData.phoneNumber} onChange={handleInputChange}/>
+
+        <label htmlFor="email">Email:</label>
+        <input type="text" id="email" name='email' value={formData.email} onChange={handleInputChange}/>
+
+        <button type="submit">Submit</button>
+        <button onClick={handleCancel}>Cancel</button>
+      </form>
     )
   }else{
     return(
-      <div>
-       <h1>{name}</h1>
-        <Image 
-        src={source}
-        alt={alt}
-        />
-        <button onClick={handleClickBtn} >
-          Edit Header
-        </button>
-        <h3>{phoneNumber}</h3>
-        <h3>{email}</h3> 
-        
-    
-     
-      </div>
+     <Output></Output>
     )
   }
 
  
-
+  function Output(){
+    return(
+      <div>
+      <h1>{name}</h1>
+       <Image 
+       src={source}
+       alt={alt}
+       />
+       <button onClick={handleClickBtn} >
+         Edit Header
+       </button>
+       <h3>{phoneNumber}</h3>
+       <h3>{email}</h3> 
+     </div>
+    )
+  }
 
   function handleClickBtn(){
     edit === true ? toggleEdit(false) : toggleEdit(true)
   }
-
- 
-  
 }
 
 
