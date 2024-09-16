ab815df3b8f7f533649eaaea446632f426805756
AaronMauget <amauget@hotmail.com>
2024-09-15 19:18:15 -0700

    Reconfig Form() to iterate data & Form components rm hardcoded FormData Create generateFormData() Call it from useState for formData
    iterate over editData to create labels & inputs

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,15 +1,18 @@
 import { useState } from "react";
 
-
 export default function Form({ editData, title, toggleEdit}){
-  const [formData, setFormData] = useState({ //temporary state to hold form data before submit
-    name: editData.name.item,
-    source: editData.source.item,
-    alt: editData.alt.item,
-    phoneNumber: editData.phoneNumber.item,
-    email: editData.email.item
-
-  })
+  //allows all components to use this Form as long as data structure is consistent
+  const generateFormData = () => { 
+    let list = {}
+    for(let i in editData){
+      let key = editData[i].key
+      list[key] = editData[key].item
+    }
+    return list
+  }
+ 
+  const[formData, setFormData] = useState(generateFormData())
+   
   const handleInputChange = (e) =>{
     const {name, value} = e.target
 
@@ -37,27 +40,26 @@ export default function Form({ editData, title, toggleEdit}){
     
     toggleEdit(false)
   }
+  
   return(
+    //Iterates over editData object so that forms of varying length can be produced.
     <form onSubmit={handleSubmit}>
       <h1>{title}</h1>
-      <label htmlFor="name">Name:</label>
-      <input type="text" id="name" name='name' value={formData.name} onChange={handleInputChange}/>
-    
-      <label htmlFor="source">Image URL:</label>
-      <input type="text" id="source" name='source' value={formData.source} onChange={handleInputChange}/>
-
-      <label htmlFor="alt">Image Alt Text:</label>
-      <input type="text" id="alt" name='alt' value={formData.alt} onChange={handleInputChange}/>
-
-      <label htmlFor="phoneNumber">Phone Number:</label>
-      <input type="text" id="phoneNumber" name='phoneNumber' value={formData.phoneNumber} onChange={handleInputChange}/>
-
-      <label htmlFor="email">Email:</label>
-      <input type="text" id="email" name='email' value={formData.email} onChange={handleInputChange}/>
-
+      {Object.entries(editData).map(([key, { item }]) => ( //Evaluate whether to keep "item"
+        <div key={key}>
+          <label htmlFor={key}>{key.charAt(0).toUpperCase() + key.slice(1)}:</label>
+          <input
+            type="text"
+            id={key}
+            name={key}
+            value={formData[key]}  // formData holds the current values
+            onChange={handleInputChange}  // BUG, this is only running for the 1st Char entered.
+          />
+        </div>
+      ))}
       <button type="submit">Submit</button>
       <button onClick={handleCancel}>Cancel</button>
     </form>
+    
   )
-
 }
